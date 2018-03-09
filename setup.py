85bae84ac826531f8f6335aed1a0b9d2677c74ea
U2328 <tim.fischer98@hotmail.com>
2018-03-09 15:22:11 +0100

    added basic package structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='STB-Liga-export',
+      version='1.0',
+      description='Easy way to export STB-Liga results to excel',
+      author='Tim Fischer & Philipp Stärk',
+     )
